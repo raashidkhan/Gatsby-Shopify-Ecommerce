52420ff34ac41297364c09fabac6580d961cf7b1
raashid <44189356+raashidkhan@users.noreply.github.com>
2020-12-06 22:29:05 +0530

    feat: Added mobile navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,32 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React, { useContext, useState } from "react"
+import { useSpring, animated } from "react-spring"
 import Logout from "../components/accounts/Logout"
 import StoreContext from "../context/store"
 import Cart from "./cartWindow"
 import Logo from "../assets/logo.svg"
+//import Cart from "../assets/cart.svg"
 import {
   Navbar,
   Menu,
   DesktopMenu,
-  MobileMenu,
   MenuButton,
+  MobileMenu,
   DesktopMenuItem,
+  MobileMenuItem,
+  CartButton,
+  MenuCloseButton,
 } from "../utils/shared"
-const Header = ({ siteTitle }) => {
+const Header = () => {
   const { customerAccessToken, isCartOpen, toggleCart, checkout } = useContext(
     StoreContext
   )
+  const [isOpen, setIsOpen] = useState(false)
+
+  const animation = useSpring({
+    opacity: isOpen ? 1 : 0,
+    pointerEvents: isOpen ? "all" : "none",
+  })
   const isAuthenticated =
     customerAccessToken &&
     customerAccessToken.expiresAt &&
@@ -29,9 +39,39 @@ const Header = ({ siteTitle }) => {
       <div className="logo">
         <img style={{ display: "block" }} src={Logo} alt="" />
       </div>
-      <Menu className="menu">
-        <MobileMenu className="mobile-menu"></MobileMenu>
-        <DesktopMenu className="desktop-menu">
+      <Menu>
+        <animated.div style={animation}>
+          <MobileMenu>
+            <MobileMenuItem>
+              <Link to="/account">Shop</Link>
+            </MobileMenuItem>
+            <MobileMenuItem>
+              <Link to="/account">Blog</Link>
+            </MobileMenuItem>
+            <MobileMenuItem>
+              <Link to="/account">Contact</Link>
+            </MobileMenuItem>
+            <MobileMenuItem>
+              {isAuthenticated ? (
+                <Link to="/account">Account</Link>
+              ) : (
+                <Link to="/account/login">Login</Link>
+              )}
+            </MobileMenuItem>
+            <MobileMenuItem>
+              {isAuthenticated ? (
+                <Logout />
+              ) : (
+                <Link to="/account/register">Sign Up</Link>
+              )}
+            </MobileMenuItem>
+            <MenuCloseButton onClick={() => setIsOpen(false)}>
+              <span className="one"></span>
+              <span className="two"></span>
+            </MenuCloseButton>
+          </MobileMenu>
+        </animated.div>
+        <DesktopMenu>
           <DesktopMenuItem>
             <Link to="/account">Shop</Link>
           </DesktopMenuItem>
@@ -56,62 +96,64 @@ const Header = ({ siteTitle }) => {
             )}
           </DesktopMenuItem>
 
-          <li>
-            <button onClick={toggleCart}>Cart</button>
-          </li>
-
           {isCartOpen && <Cart />}
         </DesktopMenu>
-        <MenuButton className="menu-btn">Menu</MenuButton>
       </Menu>
+      <CartButton onClick={toggleCart}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89.087 81.655">
+          <g
+            id="Group_7"
+            data-name="Group 7"
+            transform="translate(-1376.12 1040.655)"
+          >
+            <path
+              id="Path_5"
+              data-name="Path 5"
+              d="M1378.62-1038.155h12.642l9.114,54.686h60.272"
+              fill="none"
+              stroke="#fff"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="5"
+            />
+            <path
+              id="Path_6"
+              data-name="Path 6"
+              d="M1392.733-1028.746h69.975l-7.056,32.341-54.392,3.528Z"
+              fill="#fff"
+              stroke="#fff"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="5"
+            />
+            <g
+              id="Ellipse_11"
+              data-name="Ellipse 11"
+              transform="translate(1404 -976)"
+              fill="#fff"
+              stroke="#fff"
+              strokeWidth="1"
+            >
+              <circle cx="8.5" cy="8.5" r="8.5" stroke="none" />
+              <circle cx="8.5" cy="8.5" r="8" fill="none" />
+            </g>
+            <g
+              id="Ellipse_12"
+              data-name="Ellipse 12"
+              transform="translate(1437 -976)"
+              fill="#fff"
+              stroke="#fff"
+              strokeWidth="1"
+            >
+              <circle cx="8.5" cy="8.5" r="8.5" stroke="none" />
+              <circle cx="8.5" cy="8.5" r="8" fill="none" />
+            </g>
+          </g>
+        </svg>
+      </CartButton>
+      <MenuButton onClick={() => setIsOpen(true)}>Menu</MenuButton>
     </Navbar>
   )
 }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
 export default Header
-
-//  <header>
-//       <div>
-//         <h1
-//           style={{
-//             margin: 0,
-//             display: "flex",
-//             justifyContent: "space-between",
-//             alignItems: "center",
-//             position: "relative",
-//           }}
-//         >
-//           <Link
-//             to="/"
-//             style={{
-//               color: `white`,
-//               textDecoration: `none`,
-//             }}
-//           >
-//             <img src={Logo} alt="" />
-//           </Link>
-//           {isAuthenticated ? (
-//             <Link to="/account">Account</Link>
-//           ) : (
-//             <Link to="/account/login">Login</Link>
-//           )}
-//           {isAuthenticated ? (
-//             <Logout />
-//           ) : (
-//             <Link to="/account/register">Sign Up</Link>
-//           )}
-
-//           <button onClick={toggleCart}>Cart</button>
-
-//           {isCartOpen && <Cart />}
-//         </h1>
-//       </div>
-//     </header>
